7d20d8d63896c0fdda0eb83cd72921257bb27f09
tfanmd <ahmdtaufan122004@gmail.com>
2025-11-16 14:31:26 +0700

    CRUD Nasabah + Historis transaksi

--- app/Models/Nasabah.php
+++ app/Models/Nasabah.php
@@ -8,6 +8,7 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Nasabah extends Authenticatable
 {
@@ -27,5 +28,17 @@ class Nasabah extends Authenticatable
     {
         // Standar: 'kelas_id'
         return $this->belongsTo(Kelas::class);
+    }
+
+    // fungsi utk memberi tahukan pk nasabah itu string relasi ke transaksi (polimorfik)
+    public function tokens(): MorphMany
+    {
+        return $this->morphMany(
+            \Laravel\Sanctum\PersonalAccessToken::class,
+            'tokenable',      //nama relasi (prefix)
+            'tokenable_type', //kolom tipe di DB (e.g., 'App\Models\Nasabah')
+            'tokenable_id',   //kolom ID di DB (yang sudah kita ubah jadi string)
+            'no_rekening'     //kolom PK di tabel nasabah
+        );
     }
 }
